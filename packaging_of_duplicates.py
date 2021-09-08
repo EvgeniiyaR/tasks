b5de0a45b252c97fafd9540c4ed48c939ae1a088
EvgeniiyaR <tulpansik@mail.ru>
2021-09-08 17:13:33 +0300

    Упаковка дубликатов

---
+++ packaging_of_duplicates.py
@@ -0,0 +1,28 @@
+"""
+
+На вход программе подается строка текста, содержащая символы. Напишите программу, которая упаковывает последовательности одинаковых символов заданной строки в подсписки.
+
+Формат входных данных:
+На вход программе подается строка текста, содержащая символы, отделенные символом пробела.
+
+Формат выходных данных:
+Программа должна вывести указанный вложенный список.
+
+"""
+
+a = input().split(' ')
+new_list = []
+other_list = []
+for x in a:
+    if not other_list:
+        other_list.append(x)
+    else:
+        if other_list[-1] == x:
+            other_list.append(x)
+        else:
+            new_list.append(other_list)
+            other_list = []
+            other_list.append(x)
+if other_list:
+    new_list.append(other_list)
+print(new_list)
